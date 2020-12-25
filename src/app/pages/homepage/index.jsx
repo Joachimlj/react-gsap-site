bc72caba3a6097092829658ae1ad58ea37af31e9
Joachim Jørgensen <joachimlj.work@gmail.com>
2020-12-25 14:56:37 +0100

    added hero section to the homepage

--- src/app/pages/homepage/index.jsx
+++ src/app/pages/homepage/index.jsx
@@ -3,13 +3,21 @@ import React from "react";
 export default function Homepage() {
   return (
     <>
-      <h1>Homepage</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-        labore sed nihil illum culpa laudantium nisi ratione, sapiente, ducimus
-        vel aperiam a tenetur beatae voluptates ipsam accusantium minus
-        explicabo nesciunt?
-      </p>
+      <div className="hero">
+        <div className="hero__inner-wrapper">
+          <h1>
+            <div className="line">
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="line">
+              <p>Lorem, ipsum dolor.</p>
+            </div>
+            <div className="line">
+              <p>Lorem, ipsum dolor.</p>
+            </div>
+          </h1>
+        </div>
+      </div>
     </>
   );
 }
